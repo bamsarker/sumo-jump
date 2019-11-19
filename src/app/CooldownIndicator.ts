1f7f1aa80c7290fac075e134f5da57ac8028003d
Sam Barker <samuel.e.barker@googlemail.com>
2019-11-19 17:21:44 +0000

    added sprites

--- src/app/CooldownIndicator.ts
+++ src/app/CooldownIndicator.ts
@@ -1,11 +1,11 @@
-import * as PIXI from 'pixi.js'
-import { circleRadius } from '../config'
-import { promiseTo } from './animations'
-import { Power1 } from 'gsap/TweenMax'
+import * as PIXI from "pixi.js";
+import { circleRadius } from "../config";
+import { promiseTo } from "./animations";
+import { Power1 } from "gsap/TweenMax";
 
 export class CooldownIndicator extends PIXI.Graphics {
-  color: number
-  radius: number
+  color: number;
+  radius: number;
   constructor({
     y,
     x,
@@ -13,21 +13,23 @@ export class CooldownIndicator extends PIXI.Graphics {
     parent,
     radius = circleRadius
   }: {
-    y: number
-    x: number
-    color: number
-    parent: PIXI.DisplayObject
-    radius?: number
+    y: number;
+    x: number;
+    color: number;
+    parent: PIXI.DisplayObject;
+    radius?: number;
   }) {
-    super()
-    parent.addChild(this)
-    this.color = color
-    this.radius = radius
-    this.x = x
-    this.y = y
-    this.beginFill(this.color)
+    super();
+    parent.addChild(this);
+    this.color = color;
+    this.radius = radius;
+    this.x = x;
+    this.y = y;
+    this.beginFill(this.color);
 
-    this.drawSlice(this.x, this.y, this.radius, 360 * PIXI.DEG_TO_RAD)
+    this.visible = false;
+
+    this.drawSlice(this.x, this.y, this.radius, 360 * PIXI.DEG_TO_RAD);
   }
 
   drawSlice = (
@@ -38,25 +40,25 @@ export class CooldownIndicator extends PIXI.Graphics {
   ) =>
     this.arc(x, y, 0, sweep, 0, true)
       .arc(x, y, outerRadius, 0, sweep, false)
-      .closePath()
+      .closePath();
 
   fillUp = (duration: number) => {
-    const props = { angle: 0 }
+    const props = { angle: 0 };
     return promiseTo(props, duration, {
       angle: 360,
       ease: Power1.easeIn,
       onUpdate: () => {
-        this.clear().beginFill(this.color)
+        this.clear().beginFill(this.color);
 
         this.drawSlice(
           this.x,
           this.y,
           this.radius,
           props.angle * PIXI.DEG_TO_RAD
-        )
+        );
       }
-    })
-  }
+    });
+  };
 }
 
-export default CooldownIndicator
+export default CooldownIndicator;
--- src/app/Player.ts
+++ src/app/Player.ts
@@ -1,4 +1,5 @@
-import { Circle } from './Circle'
+import * as PIXI from "pixi.js";
+import { Circle } from "./Circle";
 import {
   circleRadius,
   playerSpeed,
@@ -7,46 +8,62 @@ import {
   circleColors,
   jumpDuration,
   knockBackDuration
-} from '../config'
-import { moveTo, grow, shake } from './animations'
-import { Linear, Circ } from 'gsap/TweenMax'
-import { Cursors, Point } from '../interfaces'
-import { ScoreText } from './ScoreText'
-import PoundEffect from './PoundEffect'
-import CooldownIndicator from './CooldownIndicator'
+} from "../config";
+import { moveTo, grow, shake } from "./animations";
+import { Linear, Circ } from "gsap/TweenMax";
+import { Cursors, Point } from "../interfaces";
+import { ScoreText } from "./ScoreText";
+import PoundEffect from "./PoundEffect";
+import CooldownIndicator from "./CooldownIndicator";
 
-export class Player extends Circle {
-  rotationSpeed: number
-  xSpeed: number
-  ySpeed: number
-  target: Circle
-  active: boolean
-  startingPos: Point
-  isGrounded: boolean
-  isKnockedBack: boolean
-  handleGroundPound: (pounder: Player) => void
-  score: number
-  scoreText: ScoreText
-  poundEffect: PoundEffect
-  jumpAvailable: boolean
-  cooldownIndicator: CooldownIndicator
-  constructor({ x, y, color, keys, handleGroundPound, scoreText }) {
-    super({ x, y, color: circleLightColors[circleColors.indexOf(color)] })
+export class Player extends PIXI.Container {
+  rotationSpeed: number;
+  xSpeed: number;
+  ySpeed: number;
+  target: Circle;
+  active: boolean;
+  startingPos: Point;
+  isGrounded: boolean;
+  isKnockedBack: boolean;
+  handleGroundPound: (pounder: Player) => void;
+  score: number;
+  scoreText: ScoreText;
+  poundEffect: PoundEffect;
+  jumpAvailable: boolean;
+  cooldownIndicator: CooldownIndicator;
+  sprite: PIXI.Sprite;
+  greySprite: PIXI.Sprite;
+  constructor({ x, y, color, keys, handleGroundPound, scoreText, sprite }) {
+    super();
 
-    this.startingPos = { x, y }
+    this.x = x;
+    this.y = y;
+
+    this.sprite = new PIXI.Sprite(PIXI.Texture.from(sprite));
+    this.sprite.anchor.set(0.5, 0.5);
+    this.sprite.scale.set(0.1);
+    this.sprite.rotation = 90 * PIXI.DEG_TO_RAD;
+
+    this.greySprite = new PIXI.Sprite(PIXI.Texture.from("grey_pants"));
+    this.greySprite.anchor.set(0.5, 0.5);
+    this.greySprite.scale.set(0.1);
+    this.greySprite.rotation = 90 * PIXI.DEG_TO_RAD;
+    this.greySprite.visible = false;
+
+    this.startingPos = { x, y };
     this.poundEffect = new PoundEffect({
       x,
       y,
       color,
       radius: circleRadius
-    })
-    this.xSpeed = 0
-    this.ySpeed = 0
-    this.score = 0
-    this.scoreText = scoreText
-    this.isGrounded = true
-    this.jumpAvailable = true
-    this.isKnockedBack = false
+    });
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+    this.score = 0;
+    this.scoreText = scoreText;
+    this.isGrounded = true;
+    this.jumpAvailable = true;
+    this.isKnockedBack = false;
 
     this.cooldownIndicator = new CooldownIndicator({
       x: 0,
@@ -54,19 +71,22 @@ export class Player extends Circle {
       parent: this,
       color,
       radius: circleRadius
-    })
+    });
 
-    this.handleGroundPound = handleGroundPound
+    this.handleGroundPound = handleGroundPound;
 
-    this.addChild(new Circle({ x: 15, y: 0, color: 0x384d48, radius: 15 }))
+    this.addChild(new Circle({ x: 15, y: 0, color: 0x384d48, radius: 15 }));
     this.target = new Circle({
       x: circleRadius * 6,
       y: 0,
       color: 0x88888800,
       radius: 5
-    })
-    this.target.alpha = 0
-    this.addChild(this.target)
+    });
+    this.target.alpha = 0;
+    this.addChild(this.target);
+
+    this.addChild(this.sprite);
+    this.addChild(this.greySprite);
 
     this.addCursors({
       up: {
@@ -89,51 +109,51 @@ export class Player extends Circle {
         downCallback: () => (this.xSpeed = playerSpeed),
         upCallback: () => (this.xSpeed = 0)
       }
-    })
+    });
 
-    document.addEventListener('keydown', e => {
+    document.addEventListener("keydown", e => {
       if (e.key === keys.jump) {
-        this.jump()
+        this.jump();
       }
-    })
+    });
   }
 
   activate = () => {
-    this.active = true
-  }
+    this.active = true;
+  };
 
   deactivate = () => {
-    this.active = false
-  }
+    this.active = false;
+  };
 
   reset = () => {
-    this.active = true
-    this.jumpAvailable = true
-    this.isGrounded = true
-    this.isKnockedBack = false
-    this.position.x = this.startingPos.x
-    this.position.y = this.startingPos.y
-  }
+    this.active = true;
+    this.jumpAvailable = true;
+    this.isGrounded = true;
+    this.isKnockedBack = false;
+    this.position.x = this.startingPos.x;
+    this.position.y = this.startingPos.y;
+  };
 
   scorePoint = () => {
-    this.score++
-    this.scoreText.setScore(this.score)
-  }
+    this.score++;
+    this.scoreText.setScore(this.score);
+  };
 
   private bringToFront = () => {
-    this.parent.setChildIndex(this, this.parent.children.length - 1)
-  }
+    this.parent.setChildIndex(this, this.parent.children.length - 1);
+  };
 
   private jump = () => {
-    if (this.isKnockedBack || !this.isGrounded || !this.jumpAvailable) return
-    this.jumpAvailable = false
-    this.bringToFront()
-    const targetGlobalPosition = this.target.getGlobalPosition()
+    if (this.isKnockedBack || !this.isGrounded || !this.jumpAvailable) return;
+    this.jumpAvailable = false;
+    this.bringToFront();
+    const targetGlobalPosition = this.target.getGlobalPosition();
     const targetPosition = {
       x: targetGlobalPosition.x - this.parent.position.x,
       y: targetGlobalPosition.y - this.parent.position.y
-    }
-    this.isGrounded = false
+    };
+    this.isGrounded = false;
     Promise.all([
       moveTo(this, jumpDuration, {
         x: targetPosition.x,
@@ -143,83 +163,85 @@ export class Player extends Circle {
       grow(this, 1.5, jumpDuration / 2, Circ.easeOut).then(() =>
         grow(this, 1, jumpDuration / 2, Circ.easeIn)
       )
-    ]).then(this.land)
-  }
+    ]).then(this.land);
+  };
 
   private land = () => {
-    this.isGrounded = true
-    this.handleGroundPound(this)
-    shake(this.parent, 3)
-    this.parent.addChildAt(this.poundEffect, 0)
-    this.poundEffect.play({ x: this.x, y: this.y })
+    this.isGrounded = true;
+    this.handleGroundPound(this);
+    shake(this.parent, 3);
+    this.parent.addChildAt(this.poundEffect, 0);
+    this.poundEffect.play({ x: this.x, y: this.y });
 
-    this.refillJumpAvailability()
-  }
+    this.refillJumpAvailability();
+  };
 
-  refillJumpAvailability = () =>
-    this.cooldownIndicator
-      .fillUp(jumpCooldownTime)
-      .then(() => (this.jumpAvailable = true))
+  refillJumpAvailability = () => {
+    this.greySprite.visible = true;
+    this.cooldownIndicator.fillUp(jumpCooldownTime).then(() => {
+      this.greySprite.visible = false;
+      this.jumpAvailable = true;
+    });
+  };
 
-  isMoving = () => this.xSpeed !== 0 || this.ySpeed !== 0
+  isMoving = () => this.xSpeed !== 0 || this.ySpeed !== 0;
 
   pushBack = (strength: number) => {
-    if (this.isKnockedBack) return
-    this.isKnockedBack = true
+    if (this.isKnockedBack) return;
+    this.isKnockedBack = true;
 
-    this.isGrounded = true
+    this.isGrounded = true;
 
-    const x = -strength * Math.cos(this.rotation) + this.x
-    const y = -strength * Math.sin(this.rotation) + this.y
+    const x = -strength * Math.cos(this.rotation) + this.x;
+    const y = -strength * Math.sin(this.rotation) + this.y;
 
     moveTo(this, knockBackDuration, {
       x,
       y
-    }).then(() => (this.isKnockedBack = false))
-    if (!this.jumpAvailable) this.refillJumpAvailability()
-  }
+    }).then(() => (this.isKnockedBack = false));
+    if (!this.jumpAvailable) this.refillJumpAvailability();
+  };
 
   private addCursors = (cursors: Cursors) => {
-    document.addEventListener('keydown', e => {
+    document.addEventListener("keydown", e => {
       Object.keys(cursors).forEach((cursorKey: string) => {
         if (e.key === cursors[cursorKey].key) {
-          e.preventDefault()
-          cursors[cursorKey].downCallback(e)
+          e.preventDefault();
+          cursors[cursorKey].downCallback(e);
         }
-      })
-    })
-    document.addEventListener('keyup', e => {
+      });
+    });
+    document.addEventListener("keyup", e => {
       Object.keys(cursors).forEach((cursorKey: string) => {
         if (e.key === cursors[cursorKey].key) {
-          cursors[cursorKey].upCallback(e)
+          cursors[cursorKey].upCallback(e);
         }
-      })
-    })
-  }
+      });
+    });
+  };
 
   private lookAt = (x: number, y: number) => {
-    this.rotation = Math.atan2(y - this.position.y, x - this.position.x)
-  }
+    this.rotation = Math.atan2(y - this.position.y, x - this.position.x);
+  };
 
   private normalizeSpeed = () => {
-    var norm = Math.sqrt(this.xSpeed * this.xSpeed + this.ySpeed * this.ySpeed)
-    if (norm != 0) {
-      // as3 return 0,0 for a point of zero length
-      this.xSpeed = (playerSpeed * this.xSpeed) / norm
-      this.ySpeed = (playerSpeed * this.ySpeed) / norm
+    if (Math.abs(this.xSpeed) + Math.abs(this.ySpeed) > playerSpeed) {
+      const div = 1.5;
+      this.xSpeed = this.xSpeed > 0 ? playerSpeed / div : -playerSpeed / div;
+      this.ySpeed = this.ySpeed > 0 ? playerSpeed / div : -playerSpeed / div;
     }
-  }
+  };
 
   update = (delta: number, opponent: Player) => {
-    if (!this.active || this.isKnockedBack || !this.isGrounded) return
+    if (!this.active || this.isKnockedBack || !this.isGrounded) return;
 
-    this.lookAt(opponent.x, opponent.y)
+    this.lookAt(opponent.x, opponent.y);
 
-    this.normalizeSpeed()
+    this.normalizeSpeed();
 
-    this.position.x += this.xSpeed * delta
-    this.position.y += this.ySpeed * delta
-  }
+    this.position.x += this.xSpeed * delta;
+    this.position.y += this.ySpeed * delta;
+  };
 }
 
-export default Player
+export default Player;
